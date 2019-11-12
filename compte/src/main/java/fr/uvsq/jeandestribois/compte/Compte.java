926972f27cd3eace22f97bcfaf669dfc3107beeb
Jean Destribois <jean.destribois@gmail.com>
2019-11-12 20:12:14 +0100

    Jean: Partie_Jean: changement de version de junit, integration de la javadoc et de l'outil checkstyle dans le pom.

--- compte/src/main/java/fr/uvsq/jeandestribois/compte/Compte.java
+++ compte/src/main/java/fr/uvsq/jeandestribois/compte/Compte.java
@@ -3,24 +3,34 @@ package fr.uvsq.jeandestribois.compte;
 public class Compte {
 
 	private int solde;
+
 	Compte() {
 		this.solde = 0;
 	}
+
 	Compte(int s) {
-		if(s<0) this.solde = 0;
-		else this.solde = s;
+		if (s < 0)
+			this.solde = 0;
+		else
+			this.solde = s;
 	}
+
 	public int getSolde() {
 		return this.solde;
 	}
+
 	public void credit(int s) {
-		if(s>0) this.solde += s;
+		if (s > 0)
+			this.solde += s;
 	}
+
 	public void debit(int s) {
-		if(s>0 && s<=this.solde) this.solde -= s;
+		if (s > 0 && s <= this.solde)
+			this.solde -= s;
 	}
+
 	public void virement(Compte c, int s) {
-		if(s>0 && s<=this.solde) {
+		if (s > 0 && s <= this.solde) {
 			this.debit(s);
 			c.credit(s);
 		}
--- compte/src/test/java/fr/uvsq/jeandestribois/compte/CompteTest.java
+++ compte/src/test/java/fr/uvsq/jeandestribois/compte/CompteTest.java
@@ -3,7 +3,7 @@ package fr.uvsq.jeandestribois.compte;
 import junit.framework.TestCase;
 
 public class CompteTest extends TestCase {
-	
+
 	public void testConstructeur() {
 		Compte c1 = new Compte();
 		Compte c2 = new Compte(0);
@@ -11,7 +11,7 @@ public class CompteTest extends TestCase {
 		assertEquals(c1.getSolde(), c2.getSolde());
 		assertEquals(c1.getSolde(), c3.getSolde());
 	}
-	
+
 	public void testCredit() {
 		Compte c1 = new Compte(20);
 		c1.credit(-1);
@@ -19,7 +19,7 @@ public class CompteTest extends TestCase {
 		c1.credit(50);
 		assertEquals(c1.getSolde(), 70);
 	}
-	
+
 	public void testDebit() {
 		Compte c1 = new Compte(20);
 		c1.debit(-1);
@@ -29,7 +29,7 @@ public class CompteTest extends TestCase {
 		c1.debit(10);
 		assertEquals(c1.getSolde(), 10);
 	}
-	
+
 	public void testVirement() {
 		Compte c1 = new Compte(20);
 		Compte c2 = new Compte(40);
